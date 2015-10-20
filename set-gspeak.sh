ab8a9feffb32ac2f29c2a44cb806dfc3f7a1fde8
bbbbbbb bbbbbbb <bbbbbbb@oooooo.com>
2015-10-20 12:10:19 -0700

    debian: added -gs3.21 suffix to package name, added set-gspeak.sh (bug 14393)

---
+++ set-gspeak.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Run this script before doing 'debuild' if you want to select a different
+# version of g-speak
+
+set -x
+set -e
+
+version=$1
+
+case "$version" in
+3.[0-9]*) ;;
+*)
+    echo "Usage: $0 G_SPEAK_VERSION"
+    echo "example: $0 3.8"
+    exit 1
+    ;;
+esac
+
+# Update the version number in all instances of the strings
+# oblong-plasma-web-proxy-gs3.X in files in debian directory
+sed -i.bak \
+    -e "s/oblong-plasma-web-proxy-gs3.[0-9][0-9]*x/oblong-plasma-web-proxy-gs${version}x/g" \
+    debian/control debian/changelog debian/postinst debian/prerm Makefile
